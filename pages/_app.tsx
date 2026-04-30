d6715fc06ee8d280ee4cb0f251b6dc53d1af7552
Madster522 <zach17732@gmail.com>
2026-04-30 16:37:51 -0400

    safrey  features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from 'react-hot-toast'
